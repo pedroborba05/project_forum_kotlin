5b8961b76fad1f79764fe189400f9ce38dabc07b
Pedro H Borba <59706583+pedroborba05@users.noreply.github.com>
2023-09-26 09:02:29 -0300

    Adicionando DTOs para controle nos service.

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,13 +1,11 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.dto.NovoTopicoDto
 import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
@@ -20,5 +18,10 @@ class TopicoController(private val service: TopicoService){
     @GetMapping("/{id}")
     fun buscarPorId(@PathVariable id: Long): Topico {
         return service.buscarPorId(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody dto: NovoTopicoDto) {
+        service.cadastrar(dto)
     }
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,65 +1,15 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.model.Curso
+import br.com.alura.forum.dto.NovoTopicoDto
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
-import java.util.*
+import kotlin.collections.ArrayList
 
 @Service
-class TopicoService(private var topicos: List<Topico>) {
-
-    init {
-        val topico = Topico(
-                id = 1,
-                titulo = "Duvida Kotlin",
-                mensagem = "Variaveis no Kotlin",
-                curso = Curso(
-                        id = 1,
-                        nome = "Kotlin",
-                        categoria = "Programacao"
-                ),
-                autor = Usuario(
-                        id = 1,
-                        nome = "Ana da Silva",
-                        email = "ana@email.com"
-                )
-        )
-
-        val topico2 = Topico(
-                id = 2,
-                titulo = "Duvida Kotlin 2",
-                mensagem = "Variaveis no Kotlin 2",
-                curso = Curso(
-                        id = 1,
-                        nome = "Kotlin",
-                        categoria = "Programacao"
-                ),
-                autor = Usuario(
-                        id = 1,
-                        nome = "Ana da Silva",
-                        email = "ana@email.com"
-                )
-        )
-
-        val topico3 = Topico(
-                id = 3,
-                titulo = "Duvida Kotlin 3",
-                mensagem = "Variaveis no Kotlin 3",
-                curso = Curso(
-                        id = 1,
-                        nome = "Kotlin",
-                        categoria = "Programacao"
-                ),
-                autor = Usuario(
-                        id = 1,
-                        nome = "Ana da Silva",
-                        email = "ana@email.com"
-                )
-        )
-
-        topicos = Arrays.asList(topico, topico2, topico3)
-    }
+class TopicoService(private var topicos: List<Topico> = ArrayList(),
+        private val cursoService: CursoService,
+        private val usuarioService: UsuarioService
+        ) {
 
     fun listar(): List<Topico> {
         return topicos
@@ -69,5 +19,15 @@ class TopicoService(private var topicos: List<Topico>) {
         return topicos.stream().filter({
             t -> t.id == id
         }).findFirst().get()
+    }
+
+    fun cadastrar(dto : NovoTopicoDto) {
+        topicos = topicos.plus(Topico(
+                id = topicos.size.toLong() + 1,
+                titulo = dto.titulo,
+                mensagem = dto.mensagem,
+                curso = cursoService.buscarPorId(dto.idCurso),
+                autor = usuarioService.buscarPorId(dto.idAutor)
+        ))
     }
 }
