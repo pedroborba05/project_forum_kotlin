9c1d266362629163dea8b9bb8f33385b96780c84
Pedro H Borba <59706583+pedroborba05@users.noreply.github.com>
2023-09-26 14:09:52 -0300

    Adicionando validações com bean validation ao topicoForm.

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -2,9 +2,9 @@ package br.com.alura.forum.controller
 
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
-import br.com.alura.forum.model.Topico
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/topicos")
@@ -19,7 +19,7 @@ class TopicoController(private val service: TopicoService){
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody dto: NovoTopicoForm) {
+    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
         service.cadastrar(dto)
     }
 }
