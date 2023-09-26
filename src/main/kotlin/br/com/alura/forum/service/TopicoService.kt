8e54c5ac9e01cb7faa3eaa7f266e5fcbb190989e
Pedro H Borba <59706583+pedroborba05@users.noreply.github.com>
2023-09-26 09:18:26 -0300

    Configurando DTOs de entrada e de saída para melhor representação dos recursos.

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,8 +1,10 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.dto.NovoTopicoDto
+import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
+import java.util.stream.Collectors
 import kotlin.collections.ArrayList
 
 @Service
@@ -11,17 +13,30 @@ class TopicoService(private var topicos: List<Topico> = ArrayList(),
         private val usuarioService: UsuarioService
         ) {
 
-    fun listar(): List<Topico> {
-        return topicos
+    fun listar(): List<TopicoView> {
+        return topicos.stream().map { t -> TopicoView(
+                id = t.id,
+                titulo = t.titulo,
+                mensagem = t.mensagem,
+                dataCriacao = t.dataCriacao,
+                status = t.status
+        ) }.collect(Collectors.toList())
     }
 
-    fun buscarPorId(id: Long): Topico {
-        return topicos.stream().filter({
-            t -> t.id == id
-        }).findFirst().get()
+    fun buscarPorId(id: Long): TopicoView {
+        val topico = topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+        return TopicoView(
+                id = topico.id,
+                titulo = topico.titulo,
+                mensagem = topico.mensagem,
+                dataCriacao = topico.dataCriacao,
+                status = topico.status
+        )
     }
 
-    fun cadastrar(dto : NovoTopicoDto) {
+    fun cadastrar(dto : NovoTopicoForm) {
         topicos = topicos.plus(Topico(
                 id = topicos.size.toLong() + 1,
                 titulo = dto.titulo,
